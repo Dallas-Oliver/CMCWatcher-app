fe9b7d22dd4780608a0759ae80ad2b24fad73c80
Dallas Oliver <dallas.oliver@everbridge.com>
2021-07-30 15:31:05 -0400

    app stuff

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const { App } = require("@slack/bolt");
 require('dotenv').config();
+const serviceHost = "http://localhost:3000"
+const port = 8001;
+const ServiceClient = require("./service-client")
+const serviceClient = new ServiceClient(serviceHost);
 
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
@@ -8,11 +12,73 @@ const app = new App({
     appToken:process.env.SLACK_APP_TOKEN
 });
 
-app.message("hello", async ({message, say}) => {
-    await say(`hello, back! ${message.user}`)
+app.command("/pricequote", async ({ command, ack, say }) => {
+    ack();
+    const serviceResponse = await serviceClient.getLatestCoinData(command.text);
+
+    if (serviceResponse[0]) {
+        await say({
+            "blocks": [
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": serviceResponse[1]
+                    },
+                    "accessory": {
+                        "type": "image",
+                        "image_url": `${serviceResponse[2]}`,
+                        "alt_text": "logo"
+                    }
+                },
+                {
+                    "type": "section",
+                    "block_id": "section567",
+                    "text": {
+                      "type": "mrkdwn",
+                      "text": "<https://coinmarketcap.com/>\n *Today's Cryptocurrency Prices by Market Cap.*"
+                    }
+                }, 
+                {
+                    "type": "divider"
+                }
+            ]
+        })
+    } else {
+        await say(serviceResponse[1]);
+    }
+
+
+})
+
+app.message("~Bitcoin-price", async ({message, say}) => {
+    const serviceResponse = await serviceClient.getLatestCoinData("bitcoin");
+    await say({
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": `*Bitcoin Price:* $${serviceResponse[0].quote.USD.price}.`
+                }
+            },
+            {
+                "type": "section",
+                "block_id": "section567",
+                "text": {
+                  "type": "mrkdwn",
+                  "text": "<https://coinmarketcap.com/>\n *Today's Cryptocurrency Prices by Market Cap.*"
+                }
+            },
+
+        ]
+    })
 });
 
 (async () => {
-    await app.start(process.env.PORT || 3000)
-    console.log('⚡️ Bolt app is running!')
+    await app.start(port)
+    console.log(`⚡️ Bolt app is running on ${port}!`)
 })();
---
+++ service-client.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch');
+
+class ServiceClient {
+    constructor(host) {
+        this.host = host;
+    }
+
+    getLatestCoinData = async (slug) => {
+        const formattedSlug = slug.toLowerCase();
+
+        try {
+            const response = await fetch(`${this.host}/latest/${formattedSlug}`, {
+                "method": "GET",
+            })
+    
+            const json = await response.json();
+
+            const name = json["coinData"][0].name
+            const price = json["coinData"][0].quote.USD.price;
+            const supply = json["coinData"][0].total_supply
+            const lastupdated = json["coinData"][0].last_updated;
+            const logoUrl =  json["logoJson"].data[Object.keys(json["logoJson"].data)[0]].logo
+    
+            return [true, `-------*${name}*------- \n *price:* $${price} \n *total supply:* ${supply} \n *last updated:* ${lastupdated}`, logoUrl];
+        } catch(err) {
+            console.log(err)
+            return [false, `Could not find info on that coin. Check your spelling!`];
+        }
+    }
+}
+
+module.exports = ServiceClient;
