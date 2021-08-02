0c4a25fefe1970c6fa2c484d288d4b30282a8f2c
Dallas Oliver <dallas.oliver@everbridge.com>
2021-08-01 22:42:39 -0400

    stuffs

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const { App } = require("@slack/bolt");
+const { default: fetch } = require("node-fetch");
 require('dotenv').config();
 const port = 8001;
 
 const serviceHost = "http://localhost:3000"
 const ServiceClient = require("./service-client")
 const serviceClient = new ServiceClient(serviceHost);
+
+
+const slackHost = "https://slack.com/api"
 
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
@@ -42,9 +46,6 @@ app.command("/pricequote", async ({ command, ack, say }) => {
                       "type": "mrkdwn",
                       "text": "<https://coinmarketcap.com/>\n *Today's Cryptocurrency Prices by Market Cap.*"
                     }
-                }, 
-                {
-                    "type": "divider"
                 }
             ]
         })
@@ -52,6 +53,93 @@ app.command("/pricequote", async ({ command, ack, say }) => {
         await say(serviceResponse[1]);
     }
 });
+
+app.command("/list", async ({ command, ack, say }) => {
+    ack();
+    const args = command.text.split(" ");
+
+    const serviceResponse = await serviceClient.getTopTen(args[0], args[1], args[2]);
+
+    if (serviceResponse[0]) {
+        if (Array.isArray(serviceResponse[1])) {
+            const blocks = serviceResponse[1].map(dataString => {
+                return {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": dataString
+                    },
+                }
+            });
+            
+            await say({
+                "blocks": [...blocks, {
+                    "type": "section",
+                    "block_id": "section567",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "<https://coinmarketcap.com/>\n *Today's Cryptocurrency Prices by Market Cap.*"
+                    }
+                }]
+            });
+        } else {
+            say("thing wasn't right")
+        }
+    } else {
+        await say(serviceResponse[1]);
+    }
+});
+
+
+
+app.command("/greendot", async ({ command, ack, say }) => {
+    ack();
+    console.log(command)
+
+    const url = `${slackHost}/dialog.open`;
+
+    const formJson = {
+        "callback_id": "ryde-46e2b0",
+        "title": "Request a Ride",
+        "submit_label": "Request",
+        "state": "Limo",
+        "elements": [
+          {
+            "type": "text",
+            "label": "Pickup Location",
+            "name": "loc_origin"
+          },
+          {
+            "type": "text",
+            "label": "Dropoff Location",
+            "name": "loc_destination"
+          }
+        ]
+      }
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            dialog: JSON.stringify(formJson),
+            trigger_id: command.trigger_id,
+            headers: {
+                'Authorization': `Bearer ${process.env.SLACK_USER_TOKEN}`
+            }
+        })
+    
+        const json = await response.json();
+        console.log(json);
+    } catch (err) {
+        say(`there was an error: ${err}`)
+    }
+});
+
+
+
+
 
 (async () => {
     await app.start(port)
--- service-client.js
+++ service-client.js
@@ -8,30 +8,62 @@ class ServiceClient {
     getLatestCoinData = async (slug) => {
         const formattedSlug = slug.toLowerCase();
 
-        try {
+        try { 
             const response = await fetch(`${this.host}/latest/${formattedSlug}`, {
                 "method": "GET",
             })
-    
+            
             const json = await response.json();
-
+            
             const name = json["coinData"][0].name
             const price = json["coinData"][0].quote.USD.price;
-
-
+            
+            
             const supply = json["coinData"][0].total_supply
             const lastupdated = json["coinData"][0].last_updated;
             const logoUrl =  json["logoJson"].data[Object.keys(json["logoJson"].data)[0]].logo
-    
+            
             return [true, `-------*${name}*------- \n *price:* $${price} \n *total supply:* ${supply} \n *last updated:* ${lastupdated}`, logoUrl];
+        
         } catch(err) {
             console.log(err)
             return [false, `Could not find info on that coin. Check your spelling!`];
         }
     }
 
-    format = (price) => {
-        
+    getTopTen = async (sortBy, sortDir, limit) => {
+        try {
+            const searchFilters = JSON.stringify({
+                sortBy: sortBy,
+                sortDir: sortDir,
+                limit: limit
+            });
+
+            const response = await fetch(`${this.host}/latest/`, {
+                "method": "POST",
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": searchFilters
+            })
+            
+            const dataList = await response.json();
+            
+            const items = dataList.map(item => `name: ${item.name} | price: $${item.quote.USD.price} | total supply: ${item.total_supply} | last updated: ${item.last_updated} \n\n`);
+            
+            return [true, items.slice(0, 10)]
+        } catch (err) {
+            console.log(err)
+            return [false, `Request failed! try again.`]
+        }
+
+
+
+
+    }
+
+    formatPrice = (price) => {
+
     }
 }
 
